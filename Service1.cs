6cec74619f70a8fd16706231b14ee532f6885eb4
Neguro <nassim.ahmedali@gmail.com>
2020-12-25 13:37:31 +0100

    correctifs sur le code.

--- Service1.cs
+++ Service1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,7 +15,10 @@ namespace GSB_gestion_cloture
 {
     public partial class Service1 : ServiceBase
     {
-        private Timer timer = new Timer(); // creation du timer pour le service
+        // pour gerer le déclanchement de notre tache avec un thread plutôt qu'un timer. 
+        private ThreadStart tsTask;
+        private Thread myTask;
+        private Bdd bdd = Bdd.getInstance();
         public Service1()
         {
             InitializeComponent();
@@ -25,36 +29,46 @@ namespace GSB_gestion_cloture
         /// <param name="args"></param>
         protected override void OnStart(string[] args)
         {
-            Bdd.getInstance().Ouvrir();
-            this.timer.Elapsed += new ElapsedEventHandler(Tasks);
-            this.timer.Interval = 5000;
-            this.timer.Start();
+            bdd.Ouvrir();
+            tsTask = new ThreadStart(TaskLoop);
+            myTask = new Thread(tsTask);
+            myTask.Start();
         }
         /// <summary>
         /// Gestion des evenement lors de la fermeture du service
         /// </summary>
         protected override void OnStop()
         {
-            Bdd.getInstance().Fermer();
-            this.timer.Stop();
+            bdd.Fermer();
+            myTask.Abort();
         }
+
+        public void TaskLoop()
+        {
+            while(true)
+            {
+                Tasks();
+                Thread.Sleep(TimeSpan.FromHours(1)); // Relance le thread toutes les heures.
+            }
+        }
+
         /// <summary>
         /// Fonction qui gére les tâches demandées 
         /// </summary>
-        public void Tasks(object source, ElapsedEventArgs e)
-        {
+        public void Tasks()
+        {       
             try
             {
                 string moisPrecedent = DateTime.Now.Year + GestionDate.getMoisPrecedent();
-                
+                // Si nous sommes entre le 1er et le 10 du mois nous démmarons la campagne de validation.
                 if(GestionDate.entre(1,10))
                 {
-                    //Bdd.getInstance().Find("fichefrais",$@"where mois={moisPrecedent}");
-                    Bdd.getInstance().Update("fichefrais", "idetat", "CL", $@"mois like '{moisPrecedent}'");
+                    bdd.Update("fichefrais", "idetat", "CL", $@"mois like '{moisPrecedent}'");
                 }
+                // Si nous somme le 20ieme ou au dela nous validons toutes les fiches du mois précèdent.
                 if(DateTime.Now.Day >= 20)
                 {
-                    Bdd.getInstance().Update("fichefrais","idetat","RB",$@"idetat = 'VA' and mois like '{moisPrecedent}'");
+                    bdd.Update("fichefrais","idetat","RB",$@"idetat = 'VA' and mois like '{moisPrecedent}'");
                 }
   
             }
