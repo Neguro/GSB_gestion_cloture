02d97f0a9319ad531776279007ed62807546154d
Neguro <nassim.ahmedali@gmail.com>
2021-01-05 14:41:07 +0100

    intégration travis CI

---
+++ assets/GSB_modifications_table.sql
@@ -0,0 +1,18 @@
+drop table if exists comptable;
+
+create table if not exists comptable (
+	id char(4) NOT NULL,
+	nom char(30) DEFAULT NULL,
+	prenom char(30)  DEFAULT NULL, 
+	login char(20) DEFAULT NULL,
+	mdp varchar(512) DEFAULT NULL,
+    primary key(id)
+) Engine=InnoDB;
+
+alter table visiteur modify mdp varchar(512);
+update visiteur set mdp = SHA2(mdp, 512);
+insert into comptable(id,nom,prenom,login,mdp) values 
+('c01','AHMED ALI','Nassim','Anas', SHA2('P@ssw0rd', 512)),
+('c02', 'LAUDE', 'Thibault', 'Lthi', SHA2('P@ssw0rd', 512)),
+('c03', 'DUSSART', 'Luke', 'Dluke', SHA2('P@ssw0rd', 512));
+select * from visiteur;
